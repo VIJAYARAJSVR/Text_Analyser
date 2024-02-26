190c805d16ad70846e8e68593ef6ee83437fc5c4
vijayaraj <Vijayfullstackdeveloper@gmail.com>
2024-02-26 19:24:31 +0400

    35% of UI design gets completed.

--- main.py
+++ main.py
@@ -160,53 +160,71 @@ class Analyzer_App:
         # Variables
         # spam = tk.StringVar()
 
-        styles = ttk.Style()
         # style applying to global
+        styles = ttk.Style()
         styles.configure('.', font=('Helvetica', 18))
 
         head_lbl = ttk.Label(text="Text Analyser", width=25, padding=15)
         head_lbl.config(font=('Courier', 35, 'bold'), foreground='yellow')
         head_lbl.pack()
 
+        path_row = ttk.Frame(root)
+        path_row.pack(fill=BOTH, expand=YES)
+
         # header and labelframe option container
         path_frame_text = 'Images are saved in below Folder'
         # self.option_lf = ttk.Labelframe(root, text=option_text, padding=15, labelanchor="n")
-        self.path_frame = ttk.Labelframe(root, text=path_frame_text, padding=15)
-        self.path_frame.pack(fill=X, expand=YES, anchor=N)
+        self.path_frame = ttk.Labelframe(path_row, text=path_frame_text, padding=15)
+        self.path_frame.pack(fill=BOTH, expand=YES, anchor=N)
 
-        path_row = ttk.Frame(self.path_frame)
-        path_row.pack(fill=X, expand=YES)
-
-        path_lbl = ttk.Label(path_row, text="Selected Folder Path :", width=25)
+        path_lbl = ttk.Label(self.path_frame, text="Selected Folder Path :", width=25)
         # path_lbl.config(foreground='blue', background='yellow')
         path_lbl.config(font=('Courier', 18, 'bold'))
-        path_lbl.pack()
+        path_lbl.pack(side=LEFT, padx=10, pady=10)
+        # path_lbl.grid(row=0, column=0)
 
-        self.selected_file_label = ttk.Label(path_row, text="", width=170)
+        self.selected_file_label = ttk.Label(self.path_frame, text="")
         folder_path = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
         self.selected_file_label.config(text=f"{folder_path}")
         self.selected_file_label.config(foreground='yellow')
         self.selected_file_label.config(font=('Courier', 25, 'bold'))
-        self.selected_file_label.pack()
+        self.selected_file_label.pack(side=LEFT, padx=10, pady=10)
+        self.selected_file_label.config(wraplength=600)
 
-        open_button = ttk.Button(path_row, text="Open File", command=self.open_file_dialog)
-        open_button.pack(padx=20, pady=20)
-        # open_button.grid(row=0,column=0,columnspan = 2)
+        # self.selected_file_label.grid(row=0, column=1)
 
-        generate_button = ttk.Button(path_row, text="Show Analysed Text", command=self.generate)
-        generate_button.pack(padx=20, pady=20)
+        generate_button = ttk.Button(self.path_frame, text="Show Analysed Text", command=self.generate)
+        generate_button.pack(side=RIGHT, padx=10, pady=10)
 
-        # ————————————————————————————————————————————-
-        content_frame_text = 'Please paste the text / paragraph to process below'
-        self.content_frame = ttk.Labelframe(root, text=content_frame_text, padding=15)
-        self.content_frame.pack(fill=X, expand=YES, anchor=N)
+        open_button = ttk.Button(self.path_frame, text="Open File", command=self.open_file_dialog)
+        open_button.pack(side=RIGHT, padx=10, pady=10)
+        # open_button.grid(row=0,column=2)
 
-        content_row = ttk.Frame(self.content_frame)
-        content_row.pack(fill=X, expand=YES)
+        # generate_button.grid(row=1, column=1)
+
+        # —————————————————————————————ui———————————————-
+
+        panedwindow = ttk.Panedwindow(root, orient=HORIZONTAL)
+        panedwindow.pack(fill=BOTH, expand=True)
+        framel = ttk.Frame(panedwindow, width=10, height=300, relief=SUNKEN)
+        frame2 = ttk.Frame(panedwindow, width=10, height=300, relief=SUNKEN)
+        frame3 = ttk.Frame(panedwindow, width=400, height=300, relief=SUNKEN)
+        panedwindow.add(framel, weight=1)
+        panedwindow.add(frame2, weight=2)
+        panedwindow.add(frame3, weight=4)
+
+        # frame3 = ttk.Frame(panedwindow, width=50, height=400, relief=SUNKEN)
+        # panedwindow.insert(1, frame3)
 
         style = ttk.Style()
+
+        content_frame_text = 'Please paste the text / paragraph to process below'
+        self.content_frame = ttk.Labelframe(framel, text=content_frame_text, padding=15)
+        self.content_frame.pack(fill=X, expand=YES, anchor=N)
+
         self.textbox = ScrolledText(
-            master=content_row,
+            width=10, height=400,
+            master=self.content_frame,
             highlightcolor=style.colors.primary,
             highlightbackground=style.colors.border,
             highlightthickness=1)
@@ -232,8 +250,8 @@ class Analyzer_App:
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     printing('**************************')
-    appWidth = 900
-    appHeight = 900
+    appWidth = 1600
+    appHeight = 1000
     # root = ttk.Window(themename="darkly", size=(800, 800), resizable=(True, True), title='Text Analyzer',
     #                   maxsize = (1300, 1300),minsize=(appWidth, appHeight), position = (400, 400))
     root = ttk.Window(themename="darkly", resizable=(True, True), title='Text Analyzer', minsize=(appWidth, appHeight))
