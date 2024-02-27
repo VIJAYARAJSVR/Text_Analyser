0c0025ec03517433cb6adfb3d2f5375561a0a686
vijayaraj <Vijayfullstackdeveloper@gmail.com>
2024-02-27 16:47:53 +0400

    Almost completed the design and implementation of common words.

--- main.py
+++ main.py
@@ -154,6 +154,54 @@ def perform_WordCloud(text_to_process, stop_words, max_wordsss, image_file_name,
     return cleaned_data_text
 
 
+class VerticalScrolledFrame(ttk.Frame):
+    """A pure Tkinter scrollable frame that actually works!
+    * Use the 'interior' attribute to place widgets inside the scrollable frame
+    * Construct and pack/place/grid normally
+    * This frame only allows vertical scrolling
+
+    """
+
+    def __init__(self, parent, *args, **kw):
+        ttk.Frame.__init__(self, parent, *args, **kw)
+
+        # create a canvas object and a vertical scrollbar for scrolling it
+        vscrollbar = ttk.Scrollbar(self, orient='vertical')
+        vscrollbar.pack(fill='y', side='right', expand='false')
+        canvas = ttk.Canvas(self, bd=0, highlightthickness=0,
+                            yscrollcommand=vscrollbar.set)
+        canvas.pack(side='left', fill='both', expand='true')
+        vscrollbar.config(command=canvas.yview)
+
+        # reset the view
+        canvas.xview_moveto(0)
+        canvas.yview_moveto(0)
+
+        # create a frame inside the canvas which will be scrolled with it
+        self.interior = interior = ttk.Frame(canvas)
+        interior_id = canvas.create_window(0, 0, window=interior,
+                                           anchor='nw')
+
+        # track changes to the canvas and frame width and sync them,
+        # also updating the scrollbar
+        def _configure_interior(event):
+            # update the scrollbars to match the size of the inner frame
+            size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
+            canvas.config(scrollregion="0 0 %s %s" % size)
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the canvas's width to fit the inner frame
+                canvas.config(width=interior.winfo_reqwidth())
+
+        interior.bind('<Configure>', _configure_interior)
+
+        def _configure_canvas(event):
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the inner frame's width to fill the canvas
+                canvas.itemconfigure(interior_id, width=canvas.winfo_width())
+
+        canvas.bind('<Configure>', _configure_canvas)
+
+
 class Analyzer_App:
     global img
     folder_path = ''
@@ -170,6 +218,10 @@ class Analyzer_App:
         self.frame2 = None
         self.textbox = None
         self.canvas = None
+        self.canvas1 = None
+        self.frame_inner = None
+        self.scrolly = None
+        self.scrollx = None
         self.colorchooser_output = None
         self.max_words = tk.IntVar()
         # self.varWidth = tk.IntVar()
@@ -259,8 +311,6 @@ class Analyzer_App:
         shortcut_lbl.pack(padx=5, pady=5, side=LEFT)
         shortcut_lbl.config(wraplength=300)
 
-
-
         path_row = ttk.Frame(root)
         path_row.pack(fill=BOTH, expand=YES)
 
@@ -270,8 +320,6 @@ class Analyzer_App:
         self.path_frame = ttk.Labelframe(path_row, text=path_frame_text, padding=15)
         self.path_frame.pack(fill=BOTH, expand=YES, anchor=N)
 
-
-
         self.selected_file_label = ttk.Label(self.path_frame, text="")
         self.folder_path = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
         self.selected_file_label.config(text=f"{self.folder_path}")
@@ -279,7 +327,6 @@ class Analyzer_App:
         self.selected_file_label.config(font=('Courier', 25, 'bold'))
         self.selected_file_label.pack(side=LEFT, padx=10, pady=5)
         self.selected_file_label.config(wraplength=600)
-
 
         shortcut_row = ttk.Frame(self.path_frame)
         shortcut_row.pack(side=RIGHT)
@@ -297,7 +344,6 @@ class Analyzer_App:
 
         open_button = ttk.Button(self.path_frame, text="Select a Folder", command=self.open_file_dialog)
         open_button.pack(side=RIGHT, padx=10, pady=5)
-
 
         # —————————————————————————————ui———————————————-
 
@@ -334,7 +380,14 @@ class Analyzer_App:
         self.textbox.pack(fill=BOTH)
         self.textbox.config(font=('Courier', 25, 'normal'))
 
-        default_txt = ""
+        default_txt = '''
+        Previously, we discussed the importance of filtration for fish tanks because it cleans up debris particles, grows beneficial bacteria, and helps create water movement and surface agitation for improved oxygenation. However, is it possible your aquarium filter is overly powerful and produces current that is too strong for your fish? Some fish have long and flowy fins, are small in size, or originated from slow-moving waterways and aren’t built to handle torrents of water. Perpetually fighting against fast flow can cause your fish to get whipped around the tank, start hiding in shelters, and potentially develop illnesses from the constant stress. So, if you own a betta fish, goldfish, cherry shrimp, or other slow-swimming animal, consider implementing one of these techniques to reduce the current in your aquarium.
+
+Use a Filter with Slow Flow
+The simplest way to reduce the current is to not use too much filtration in your aquarium. In their quest to have the cleanest tank possible, people sometimes install multiple filters or get oversized filters that are meant for much bigger fish tanks. Other times, newcomers to the hobby pick up an all-in-one aquarium kit and don’t realize that the default filter is too strong for bettas and other slower fish. If you see your fish struggling, don’t be afraid to downsize your filter to better accommodate their needs.
+
+Our favorite type of filtration for gentle flow is a sponge filter with a smaller or adjustable air pump. Its coarse foam is perfect for straining debris from the water without sucking up any baby fish, and the bubbles create good surface agitation to ensure your fish get enough oxygen. Some air pumps come with adjustable flow controls to lessen the air pressure if needed, but if the pump isn’t adjustable, you can also add an air valve outside of the fish tank to reduce the amount of bubbling. If you prefer to use another type of filtration like a hang-on-back or canister filter, check to see if it has an adjustable switch or knob that allows you to modify the flow rate of the water entering the aquarium.'''
+
         self.textbox.insert(END, default_txt)
 
         tabBar = ttk.Notebook(frame3)
@@ -390,24 +443,75 @@ class Analyzer_App:
 
             common_words_tuple = analyze_word_frequency(cleaned_data_text, self.varFrequency.get())
 
-            # create a scrollbar widget and set its command to the text widget
-            # scrollbar = ttk.Scrollbar(root, orient='vertical', command = self.canvas.yview)
-            # scrollbar.grid(row=0, column=1, sticky=tk.NS)
+            # style = ttk.Style()
+            # # style.theme_use('classic')
+            # style.configure("Vertical.TScrollbar", background="green", bordercolor="red", arrowcolor="white")
 
-            # scrollbar = ttk.Scrollbar(frame11, orient=VERTICAL)
-            # scrollbar.pack(fill=BOTH, expand=YES)
+            # deleting all child widgets
+            for item in self.frame2.winfo_children():
+                item.destroy()
 
-            # scrollbar = ttk.Scrollbar(self.frame2, orient='vertical', command=self.frame2)
-            # scrollbar.pack(fill=BOTH, expand=YES)
+            # self.canvas1 = ttk.Canvas(self.frame2, background="#ffffff", borderwidth=0)
+            # self.frame_inner = ttk.Frame(self.canvas1)
+            # self.scrolly = ttk.Scrollbar(self.frame2, orient="vertical", command=self.canvas1.yview)
+            # self.scrollx = ttk.Scrollbar(self.frame2, orient="horizontal", command=self.canvas1.xview)
+            # self.canvas1.configure(yscrollcommand=self.scrolly.set)
+            # self.canvas1.create_window((4, 4), window=self.frame_inner, anchor="nw", tags="self.frame_inner")
+            #
+            # self.scrolly.pack(side="left", fill="y")
+            # self.canvas1.pack(side="top", fill="both", expand=True)
+            # self.scrollx.pack(side="bottom", fill="x")
+            # self.canvas1.configure(scrollregion=self.canvas.bbox("all"))
 
-            # textbox1 = ScrolledText(
-            #     width=10, height=400,
-            #     master=self.frame2,
-            #     highlightcolor='blue',
-            #     highlightbackground='white',
-            #     highlightthickness=1)
-            # textbox1.pack(fill=BOTH)
-            # textbox1.config(font=('Courier', 25, 'normal'))
+            # self.frame_inner.bind("", self.onFrameConfigure)
+            #
+            # self.scrolly.set(0.0, 0.5)
+            # CheckVar11 = tk.IntVar()
+            # scrollbar1 = ttk.Scrollbar(self.frame2)
+            # scrollbar1.pack(side=RIGHT, fill=Y)
+            # pavanlist1 = ttk.Treeview(self.frame2, yscrollcommand=scrollbar1.set)
+            # for pavanline1 in range(100):
+            #     pavanlist1.insert(END, "Line Numbers are " + str(pavanline1) + " now in online ")
+            #     if pavanline1 % 10 == 0:
+            #         C11 = ttk.Checkbutton(self.frame2, text="Line Number : " + str(pavanline1), variable=CheckVar11)
+            #         C11.pack()
+            # pavanlist1.pack(side=LEFT, fill=BOTH)
+            # scrollbar1.config(command=pavanlist1.yview)
+
+            # scrollbar = ttk.Scrollbar(self.frame2)
+            # listbox = ttk.Treeview(self.frame2, yscrollcommand=scrollbar.set, show="tree")
+            # scrollbar.configure(command=listbox.yview)
+            #
+            # scrollbar.pack(side="right", fill="y")
+            # listbox.pack(side="left", fill="both", expand=True)
+
+            # for i in range(100):
+            #     text = f"Item #{i + 1}"
+            #     listbox.insert("", "end", text=text)
+
+            # scrollbar = ttk.Scrollbar(root)
+            # scrollbar.pack(side=RIGHT, fill=Y)
+            # scrollbar.config(command=self.frame2.yview)
+
+            # mainframe to make a scrollable window
+            self.mainframe = VerticalScrolledFrame(self.frame2)
+            self.mainframe.grid(row=0, column=0)
+            self.mainframe.pack(fill=BOTH, expand=1)
+
+            # create a notebook
+            self.TNotebook_Overview = ttk.Notebook(self.mainframe.interior)
+            self.TNotebook_Overview.grid(row=0, column=0)
+            self.TNotebook_Overview.configure(takefocus="")
+
+            self.Frame_Overview = ttk.Frame(self.TNotebook_Overview)
+
+            self.TNotebook_Overview.add(self.Frame_Overview)
+            self.TNotebook_Overview.tab(0, text="Common Words", compound="left", underline="-1", )
+
+            # buttons = []
+            # for i in range(300):
+            #     buttons.append(ttk.Button(self.Frame_Overview, text="Button " + str(i)))
+            #     buttons[-1].grid(column=0, row=i)
 
             rows = []
             for rrow in range(self.varFrequency.get()):
@@ -415,19 +519,21 @@ class Analyzer_App:
                 for ccolumn in range(2):
                     if ccolumn == 0:
                         vall = common_words_tuple[rrow]
-                        word_entry = ttk.Entry(self.frame2, width=9, font='Arial 30')
-                        word_entry.grid(row=rrow, column=ccolumn, sticky=NSEW)
+                        word_entry = ttk.Entry(self.Frame_Overview, width=9, font='Arial 30')
+                        word_entry.grid(row=rrow, column=ccolumn)
                         word_entry.insert(END, (vall[0]))
                         cols.append(word_entry)
                         rows.append(cols)
-
                     else:
                         vall = common_words_tuple[rrow]
-                        count_lbl = ttk.Label(self.frame2, text=vall[-1], width=10, font='Arial 25')
+                        count_lbl = ttk.Label(self.Frame_Overview, text=vall[-1], width=10, font='Arial 25')
                         # count_lbl.config(padx=5)
-                        count_lbl.grid(row=rrow, column=ccolumn, sticky=NSEW)
+                        count_lbl.grid(row=rrow, column=ccolumn)
                         cols.append(count_lbl)
                         rows.append(cols)
+
+    def onFrameConfigure(self, event):
+        self.canvas1.configure(scrollregion=self.canvas1.bbox("all"))
 
     def validateFields(self):
         # if len(self.textData.strip) <= 0:
